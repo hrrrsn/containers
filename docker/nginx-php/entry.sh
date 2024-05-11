5ac5b56da12c5fda4ada2b5ed7c85a36ba8b4f5f
Harrison Gulliver <harrison.gulliver@spark.co.nz>
2024-05-12 00:29:51 +1200

    nginx-php: update entry

--- docker/nginx-php/entry.sh
+++ docker/nginx-php/entry.sh
@@ -2,6 +2,8 @@
 : "${PORT:=8080}"
 : "${DOCUMENT_ROOT:=/usr/share/nginx/html}"
 
-envsubst '${PORT},${DOCUMENT_ROOT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${PORT},${DOCUMENT_ROOT}' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf.1
+
+mv /etc/nginx/nginx.conf.1 /etc/nginx/nginx.conf
 
 /usr/sbin/php-fpm -D && nginx -g "daemon off;"
