e741fc46ce83fe43442cc0defb13ef3befa69da1
Harrison Gulliver <harrison.gulliver@spark.co.nz>
2025-03-06 12:59:12 +1300

    Update actions-runner token retrieval

--- containers/actions-runner/start.sh
+++ containers/actions-runner/start.sh
@@ -6,11 +6,13 @@ echo "----------------------------------------"
 
 cd /opt/actions-runner
 
-./config.sh --url https://github.com/${REPO} --token ${TOKEN} --disableupdate --unattended
+REG_TOKEN=$(curl -X POST -H "Authorization: Bearer ${TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
+
+./config.sh --url https://github.com/${REPO} --token ${REG_TOKEN} --disableupdate --unattended
 
 cleanup() {
     echo "Removing runner..."
-    ./config.sh remove --token ${TOKEN}
+    ./config.sh remove --token ${REG_TOKEN}
 }
 
 trap 'cleanup; exit 130' INT
