f7cf6f6208c7800d85b95ad44d3ac92f92cf199a
Harrison Gulliver <harrison.gulliver@spark.co.nz>
2025-03-07 16:36:28 +1300

    fix(actions-runner): add sudo to container

--- containers/actions-runner/start.sh
+++ containers/actions-runner/start.sh
@@ -4,7 +4,7 @@ echo "User: $(whoami) (UID: $(id -u), GID: $(id -g))"
 echo "Repository: ${REPO}"
 echo "----------------------------------------"
 
-cd /opt/actions-runner
+cd /home/podman/actions-runner
 
 REG_TOKEN=$(curl -X POST -H "Authorization: Bearer ${TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
 
