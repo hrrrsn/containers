8a14537767ef84eb1d18a289745d50b80a94b781
Harrison Gulliver <harrison.gulliver@spark.co.nz>
2026-02-25 15:49:07 +1300

    fix: update tags.sh to format date output and include Fedora version

--- containers/toolbox/tags.sh
+++ containers/toolbox/tags.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 date=$(date +%Y%m%d)
 fedora_ver=$(awk '{print $3}' /etc/fedora-release)
+echo $(date +%Y-%m-%d)
+echo $(date +%Y-%m-%d)-fc$fedora_ver
 echo fc$fedora_ver
-echo fc$fedora_ver-$date
